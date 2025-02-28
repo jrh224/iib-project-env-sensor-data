4282228e88a88e78562235375fb72f11e16d7880
jameshousden0 <jameshousden0@gmail.com>
2025-02-28 11:31:19 +0000

    adding enc dec training

--- Clean Train and Predict Workflow/models.py
+++ Clean Train and Predict Workflow/models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from config import INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE
 
@@ -29,4 +30,26 @@ class Seq2SeqLSTM(nn.Module):
         output, _ = self.lstm(input) # no need for _ hidden state
         last_output = output[:, -1, :] # pass in final lstm value to linear layer
         output = self.linear(last_output)
+        return output
+    
+
+
+# Encoder/decoder architecture to allow for the inclusion of future covariates
+# Based on papers linked from https://stackoverflow.com/questions/70361179/how-to-include-future-values-in-a-time-series-prediction-of-a-rnn-in-keras
+class Seq2SeqLSTMEncDec(nn.Module):
+    def __init__(self, input_dim=INPUT_SIZE, hidden_dim=HIDDEN_SIZE, output_dim=OUTPUT_SIZE):
+        super(Seq2SeqLSTMEncDec, self).__init__()
+        self.encoder_lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.decoder_lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, encoder_inputs, decoder_inputs):
+        # Encode the input sequence (hidden and cell states of every element in the sequence) https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+        _, (hidden, cell) = self.encoder_lstm(encoder_inputs)
+
+        # Decode using an LSTM
+        decoder_outputs, _ = self.decoder_lstm(decoder_inputs, (hidden, cell))
+
+        # Fully connected layer for final output
+        output = self.fc(decoder_outputs)
         return output
